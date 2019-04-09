92eb0bc7b2379fd3f49e2ddceb1ae94a35dfc3ac
hamidreza <hamid20tik@gmail.com>
2019-04-09 16:49:25 +0430

    bulid app job  & blog and first template and run on mysql db

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -118,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,6 @@
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def ss(requset):
-    return HttpResponse("hello")
+def homepage(requset):
+    return render(requset,'jobs/home.html',)
