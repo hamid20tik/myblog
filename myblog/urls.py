aedbfeee51d66016a7e528c80ec2d1b6c969e356
hamidreza <hamid20tik@gmail.com>
2019-04-10 15:43:04 +0430

    repair urls jobs

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.homepage, name='homepage'),
+    path('',jobs.views.homepage, name='homepage'),
 ] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
--- myblog/views.py
+++ myblog/views.py
@@ -2,5 +2,3 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(requset):
-    return render(requset,'jobs/home.html',)
