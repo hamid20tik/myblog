dff4fb8becaede6cabae58dbf2ac72c6b7169b1b
hamidreza <hamid20tik@gmail.com>
2019-04-11 19:49:45 +0430

    fix jobs and show info into home page with for loop

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import job
 def homepage(requset):
-    return render(requset,'jobs/home.html')
+    jobs = job.objects
+    return render(requset,'jobs/home.html',{'jobs':jobs})
