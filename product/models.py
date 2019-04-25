b131c3c8682fb29e1fb2fb7191894978f6ff78c8
hamidreza <hamid20tik@gmail.com>
2019-04-25 12:51:52 +0430

    لینک

---
+++ product/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class product(models.Model):
+    title = models.CharField(max_length=225)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date_pretty('%b %e %Y')
